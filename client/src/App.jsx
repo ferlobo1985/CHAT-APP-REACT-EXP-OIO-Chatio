e2ccfe905f12ebdfcb114cd058ca4d3b6eca4039
Coding rev <fernando.lobo.prez@gmail.com>
2025-07-27 21:32:02 -0300

    04-Creating the main layout
    04-Creating the main layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 import { useEffect } from "react"
+import MainLayout from "./components/mainLayout"
 
 function App() {
  
-  useEffect(()=>{
-    fetch('http://localhost:5004/')
-    .then(response=> response.json())
-    .then(data=> console.log(data))
-  },[])
+  // useEffect(()=>{
+  //   fetch('http://localhost:5004/')
+  //   .then(response=> response.json())
+  //   .then(data=> console.log(data))
+  // },[])
 
   return (
     <>
-      HELLO
+      <MainLayout/>
     </>
   )
 }
---
+++ client/src/components/mainLayout.jsx
@@ -0,0 +1,17 @@
+
+
+export default function MainLayout() {
+  return (
+    <div className="--dark-theme" id="chat">
+        <div className="chat_box">
+            chat panel
+        </div>
+        <div className="chat_panel">
+            input
+        </div>
+        <div className="side_panel">
+            side
+        </div>
+    </div>
+  )
+}
