fa43fc1dc39bae2d3f0b79ed714c28a3a89d06c8
Coding rev <fernando.lobo.prez@gmail.com>
2025-07-28 15:46:31 -0300

    11-Controlling the input text
    11-Controlling the input text

--- client/src/components/bottomPanel/InputMessage.jsx
+++ client/src/components/bottomPanel/InputMessage.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react"
 
 export default function InputMessage() {
+  const [content,setContent] = useState("");
+
+
+  const sendMessage = () => {
+    console.log(content)
+
+  }
+
+  const handleEnterPress = (e) => {
+    if(e.key === 'Enter' && content.trim()){
+      sendMessage()
+    }
+  }
+
   return (
     <div className="chat_box_panel">
         <input
             type="text"
-            value=''
-            onChange={()=>{}}
+            value={content}
+            onChange={(e)=>setContent(e.target.value)}
+            onKeyDown={handleEnterPress}
             placeholder="Chat with AI"
             className="chat_box_input"
         />
