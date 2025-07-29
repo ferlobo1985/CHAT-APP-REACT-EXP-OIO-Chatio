676071079c2d5ab1cc37ce74fe16c00542518fc1
Coding rev <fernando.lobo.prez@gmail.com>
2025-07-29 16:52:43 -0300

    22-Displaying messages part two
    22-Displaying messages part two

--- client/src/components/chat/ChatBubble.jsx
+++ client/src/components/chat/ChatBubble.jsx
@@ -4,20 +4,24 @@ import { RiRobot3Fill } from 'react-icons/ri'
 
 export default function ChatBubble({data}) {
 
-    console.log(data)
 
   return (
     <div
-        className='chat_box_container'
+        className={`chat_box_container ${data.role === 'user'?'user':'reversed'}`}
     >
         <div className='person'>
             <div className='person_avatar'>
-                <HiUserCircle/>
+                {data.role === 'user' ?
+                    <HiUserCircle/>
+                    :
+                    <RiRobot3Fill/>
+                }
+                
             </div>
         </div>
         <div className='chat_box_context'>
             <div className='chat_box_bubble'>
-                <span>Hello, this is a fake content</span>
+                <span>{data.content}</span>
             </div>
         </div>
     </div>
